c65880458c34671c3c81501a5bb6f1943f2b950f
Sarika Malhotra <Sarika.Malhotra@aexp.com>
2020-07-13 09:24:59 -0400

    commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,19 +3,18 @@ import React, { Component } from "react";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
   render() {
     return (
       <div>
         <div className="QuizQuestion">
-          {" "}
-          {quizData.quiz_questions[0].instruction_text}{" "}
+          {quizData.quiz_questions[0].instruction_text}
         </div>
       </div>
     );
-  }
-  constructor(props) {
-    super(props);
-    this.state = { quiz_position: 1 };
   }
 }
 export default Quiz;
